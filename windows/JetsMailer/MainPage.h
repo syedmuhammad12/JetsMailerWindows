1673829e6a0d810573aa5d7f3a03837b752665e3
Syed Muhammad <syedmuhammad1111@gmail.com>
2024-05-26 13:54:19 +0500

    initialization of project

---
+++ windows/JetsMailer/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::JetsMailer::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::JetsMailer::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
